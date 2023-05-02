eb00734c2f061e89adb66bbf0270b62ffd738171
riavarghese21 <130704385+riavarghese21@users.noreply.github.com>
2023-05-02 16:10:51 -0400

    Update Server.java

--- src/TCP/Server.java
+++ src/TCP/Server.java
@@ -1,99 +1,69 @@
-package TCP;
 import java.net.*;
 import java.io.*;
 
-public class TCPServer 
-{
-	private Socket socket = null;
-	private ServerSocket server;
-	private DataInputStream userInput;
+public class TCPServer {
+    private ServerSocket server;
+    private Socket socket1;
+    private Socket socket2;
+    private BufferedReader reader1;
+    private BufferedReader reader2;
+    private PrintWriter writer1;
+    private PrintWriter writer2;
 
-	public Socket getSocket()
-    	{
-        	return socket;
-    	} 
+    public TCPServer(int portNumber) throws IOException {
+        server = new ServerSocket(portNumber);
+        System.out.println("Server is running on port " + portNumber);
+
+        System.out.println("Waiting for client 1...");
+        socket1 = server.accept();
+        System.out.println("Client 1 connected from " + socket1.getInetAddress());
+
+        System.out.println("Waiting for client 2...");
+        socket2 = server.accept();
+        System.out.println("Client 2 connected from " + socket2.getInetAddress());
+
+        reader1 = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+        writer1 = new PrintWriter(socket1.getOutputStream(), true);
+
+        reader2 = new BufferedReader(new InputStreamReader(socket2.getInputStream()));
+        writer2 = new PrintWriter(socket2.getOutputStream(), true);
+
+        new Thread(new MessageTransfer(reader1, writer2, reader2, writer1)).start();
+        new Thread(new MessageTransfer(reader2, writer1, reader1, writer2)).start();
+    }
+
+    private class MessageTransfer implements Runnable {
+        private BufferedReader readerFrom;
+        private PrintWriter writerTo;
+        private BufferedReader readerOther;
+        private PrintWriter writerOther;
     
-    	public void setSocket(Socket socket) 
-   	 {
-        	this.socket = socket;
-    	}
 
-	public ServerSocket getServer() 
-	{
-		return server;
-	}
+        public MessageTransfer(BufferedReader readerFrom, PrintWriter writerTo, BufferedReader readerOther, PrintWriter writerOther) {
+            this.readerFrom = readerFrom;
+            this.writerTo = writerTo;
+            this.readerOther = readerOther;
+            this.writerOther = writerOther;
+        }	
+        
+        public void run() {
+            try {
+                String message;
+                while ((message = readerFrom.readLine()) != null) {
+                    System.out.println("Message from client: " + message);
+                    writerOther.println(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
+        }
 
-	public void setServer(ServerSocket server) 
-	{
-		this.server = server;
-	}
-
-	public DataInputStream getuserInput() 
-    	{
-        	return userInput;
-   	 }
-
-    	public void setuserInput(DataInputStream userInput) 
-   	 {
-        	this.userInput = userInput;
-    	}
-
-    public TCPServer(int portNumber) 
-	{
-		try (ServerSocket server = new ServerSocket(portNumber)) 
-		{
-			System.out.println("Server connected! ");
-			System.out.println("Client waiting . . .");
-		
-			try (Socket socket = server.accept())
-			{
-				System.out.println("client accepting server connection. ");
-		
-				try (DataInputStream userInput = new DataInputStream(
-						new BufferedInputStream(socket.getInputStream()))) 
-				{
-				String message;
-				while (true)
-				{
-					try 
-					{
-						message = userInput.readUTF();
-						if (message.equals("terminate")) 
-						{
-							break;
-						}
-						System.out.println(message);
-						} catch (IOException e) 
-						{
-							System.out.println("input error : " + e.getMessage());
-									break;
-						}
-					}
-				}
-			} catch (IOException e) {
-				System.out.println("could not configure client server. : " + e.getMessage());
-			}
-		} catch (IOException e) 
-		{
-			System.out.println("error occurred when starting server! : " + e.getMessage());
-		}
-		try {
-			if (socket != null) 
-			{
-				socket.close();
-			}
-			if (userInput != null) 
-			{
-				userInput.close();
-			}
-		} catch(IOException e) 
-		{
-			System.out.println("closing connection error " + e.getMessage());
-		}
-	}
-	//local host: 3066//
-	public static void main(String args[]) 
-	{
-			new TCPServer(3066);
-	}
+    public static void main(String[] args) throws IOException {
+        try{
+            new TCPServer(3066);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
